e6ec1dd9b1f5befec46fac8272bee18766c17ef2
Ahsan ali <120973691+AhsanAlijs@users.noreply.github.com>
2024-01-07 00:21:59 +0500

    create firebase start

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+
+
+
+
+const form = document.querySelector('#form');
+const fname = document.querySelector('#fname');
+const lname = document.querySelector('#lname');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const repassword = document.querySelector('#re-password');
+const uploadPhoto = document.querySelector('#upload-photo')
+const icons = document.querySelector('#icon')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userName = `${fname.value} ${lname.value}`
+
+    try {
+        if (password.value !== repassword.value) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Something went wrong!",
+                footer: '<a href="#">Why do I have this issue?</a>'
+                
+              });
+            
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }finally{
+
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const icon = document.querySelector('.icon');
 const menu = document.querySelector('nav');
 
@@ -8,5 +63,5 @@ icon.addEventListener('click', () => {
     } else {
         document.getElementById('bar').className = "fa-solid fa-bars";
     }
-    console.log(menu);
+    // console.log(menu);
 });
